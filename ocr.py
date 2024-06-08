041a842ed87b6911d640b29ac090f0501161f4d0
Arek <arek.kocowicz@gmail.com>
2024-06-08 08:06:36 +0200

    ocr.py modified to simplify searching for good preprocessing parameters for tesseract

--- ocr.py
+++ ocr.py
@@ -3,26 +3,35 @@ import pytesseract
 import argparse
 
 window_title="ocr"
+trackbar_resize_title="size"
+trackbar_threshold_title="threshold"
 
 def main(image):
     global src
     src = cv.imread(cv.samples.findFile(image))
+    print(src.shape)
     if src is None:
         print('Could not open or find the image: ', image)
         exit(0)
+    gray_image = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
+    src=gray_image
     cv.namedWindow(window_title)
-    #cv.createTrackbar(title_trackbar_element_shape, title_erosion_window, 0, max_elem, erosion)
-    #cv.createTrackbar(title_trackbar_kernel_size, title_erosion_window, 0, max_kernel_size, erosion)
-    #cv.namedWindow(title_dilation_window)
-    #cv.createTrackbar(title_trackbar_element_shape, title_dilation_window, 0, max_elem, dilatation)
-    #cv.createTrackbar(title_trackbar_kernel_size, title_dilation_window, 0, max_kernel_size, dilatation)
-    #erosion(0)
-    #dilatation(0)
+    cv.createTrackbar(trackbar_resize_title, window_title, 300, 1000, doOCR)
+    cv.createTrackbar(trackbar_threshold_title, window_title, 200, 255, doOCR )
     cv.imshow(window_title, src)
     print(pytesseract.image_to_string(src, config="--psm 7"))
-    print(pytesseract.image_to_boxes(src))
     cv.waitKey()
 
+def doOCR(val):
+    scale=cv.getTrackbarPos(trackbar_resize_title, window_title)
+    #print(f"doOCR {scale}")
+    fscale=scale/100
+    thres=cv.getTrackbarPos(trackbar_threshold_title, window_title)
+    resized=cv.resize(src, (0, 0), fx = fscale ,fy = fscale)
+    _, binary = cv.threshold(resized, thres, 255, cv.THRESH_BINARY)
+    cv.imshow(window_title, binary)
+    print(pytesseract.image_to_string(binary, config="--psm 7"))
+    
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Code for testing pytesseract')
